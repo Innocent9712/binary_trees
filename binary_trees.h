c337bd897ea996bc7c9e36a6ddc01775a17cf229
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-09-04 11:23:19 +0100

    Add header file

--- binary_trees.h
+++ binary_trees.h
@@ -61,5 +61,7 @@ int is_complete(const binary_tree_t *tree, unsigned int index,
 		unsigned int number_of_nodes);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+int binary_tree_is_bst(const binary_tree_t *tree);
+bst_t *bst_insert(bst_t **tree, int value);
 
 #endif /* BINARY_TREES_H */
